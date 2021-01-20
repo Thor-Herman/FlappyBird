00085618ec0aee38f7322535b505a40ccbe271f5
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-01-20 12:17:19 +0100

    Replace standard render with MenuState

--- core/src/com/flappybird/game/FlappyBird.java
+++ core/src/com/flappybird/game/FlappyBird.java
@@ -5,34 +5,39 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.flappybird.game.States.GameStateManager;
+import com.flappybird.game.States.MenuState;
+import com.flappybird.game.States.State;
 
 public class FlappyBird extends ApplicationAdapter {
 	public static final int WIDTH = 480;
 	public static final int HEIGHT = 800;
 
 	public static final String TITLE = "Flappy Bird";
-
-	SpriteBatch batch;
+	private GameStateManager gsm;
+	private SpriteBatch sb; // Should only have a single sb
 	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		sb = new SpriteBatch();
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		State menuState = new MenuState(gsm);
+		gsm.push(menuState);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Wipes screen and redraws everything
+		float dt = Gdx.graphics.getDeltaTime();
+		gsm.update(dt);
+		gsm.render(this.sb);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		sb.dispose();
 		img.dispose();
 	}
 }

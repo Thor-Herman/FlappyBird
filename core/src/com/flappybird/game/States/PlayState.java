62976cccbda47a99e99ebf8fe7984bc7f891534f
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-01-20 12:41:53 +0100

    Adjust PlayState camera viewport
    Focuses on a smaller portion of the screen

--- core/src/com/flappybird/game/States/PlayState.java
+++ core/src/com/flappybird/game/States/PlayState.java
@@ -2,6 +2,7 @@ package com.flappybird.game.States;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.flappybird.game.FlappyBird;
 
 public class PlayState extends State {
 
@@ -10,6 +11,7 @@ public class PlayState extends State {
     protected PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Texture("bird.png");
+        cam.setToOrtho(false, FlappyBird.WIDTH / 2, FlappyBird.HEIGHT / 2);
     }
 
     @Override
@@ -24,6 +26,7 @@ public class PlayState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined); 
         sb.begin();
         sb.draw(bird, 50, 50);
         sb.end();

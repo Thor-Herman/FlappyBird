9fbfe372a7e3cf0faebd0bfd96df307e6a8bdcb2
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-01-20 16:24:45 +0100

    Add colliders to Pipe

--- core/src/com/flappybird/game/sprites/Pipe.java
+++ core/src/com/flappybird/game/sprites/Pipe.java
@@ -1,6 +1,7 @@
 package com.flappybird.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 import java.util.Random;
@@ -11,22 +12,37 @@ public class Pipe {
     public static final int PIPE_GAP = 100;
     public static final int LOWEST_OPENING = 120;
 
-    private Texture topPipe, bottomPipe;
+    private Texture topPipe, botPipe;
     private Vector2 posTopPipe, posBotPipe;
+    private Rectangle boundsTop, boundsBot;
     private Random rand;
 
     public Pipe(float x) {
         topPipe = new Texture("toptube.png");
-        bottomPipe = new Texture("bottomtube.png");
+        botPipe = new Texture("bottomtube.png");
         rand = new Random();
-        this.reposition(x);
+
+        float topPipeY = rand.nextInt(FLUCTUATION) + PIPE_GAP + LOWEST_OPENING;
+        float botPipeY = topPipeY - PIPE_GAP - botPipe.getHeight();
+        this.posTopPipe = new Vector2(x, topPipeY);
+        this.posBotPipe = new Vector2(x, botPipeY);
+
+        this.boundsTop = new Rectangle(posTopPipe.x, posTopPipe.y, topPipe.getWidth(), topPipe.getHeight());
+        this.boundsBot = new Rectangle(posBotPipe.x, posBotPipe.y, botPipe.getWidth(), botPipe.getHeight());
     }
 
     public void reposition(float x) {
         float topPipeY = rand.nextInt(FLUCTUATION) + PIPE_GAP + LOWEST_OPENING;
-        float botPipeY = topPipeY - PIPE_GAP - bottomPipe.getHeight();
-        this.posTopPipe = new Vector2(x, topPipeY);
-        this.posBotPipe = new Vector2(x, botPipeY);
+        float botPipeY = topPipeY - PIPE_GAP - botPipe.getHeight();
+
+        this.posTopPipe.set(x, topPipeY);
+        this.posBotPipe.set(x, botPipeY);
+        this.boundsTop.setPosition(x, topPipeY);
+        this.boundsBot.setPosition(x, botPipeY);
+    }
+
+    public boolean collides(Rectangle player) {
+        return player.overlaps(boundsTop) || player.overlaps(boundsBot);
     }
 
     public Texture getTopPipe() {
@@ -34,7 +50,7 @@ public class Pipe {
     }
 
     public Texture getBotPipe() {
-        return bottomPipe;
+        return botPipe;
     }
 
     public Vector2 getPosTopPipe() {

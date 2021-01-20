71b27e6bb6e4907c3a0fde843c39c5df5b8397e7
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-01-20 12:10:25 +0100

    Create GameStateManager.java
    Basically a stack maintaining current state of the game

---
+++ core/src/com/flappybird/game/States/GameStateManager.java
@@ -0,0 +1,35 @@
+package com.flappybird.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
+public class GameStateManager {
+
+    private Stack<State> states;
+
+    public GameStateManager() {
+        this.states = new Stack<> ();
+    }
+
+    public void push(State state) {
+        states.push(state);
+    }
+
+    public void pop() {
+        states.pop();
+    }
+
+    public void setState(State state) {
+        this.pop();
+        this.push(state);
+    }
+
+    public void update(float dt) {
+        this.states.peek().update(dt);
+    }
+
+    public void render(SpriteBatch sb) {
+        this.states.peek().render(sb);
+    }
+}
